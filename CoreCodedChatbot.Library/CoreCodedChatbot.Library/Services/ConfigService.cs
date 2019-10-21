b5469cfc09d28bf80e286349a958361a6b2f777e
codedghost2 <codedghost2@gmail.com>
2019-10-21 16:46:22 +0100

    Making config much more generic

--- CoreCodedChatbot.Library/CoreCodedChatbot.Library/Services/ConfigService.cs
+++ CoreCodedChatbot.Library/CoreCodedChatbot.Library/Services/ConfigService.cs
@@ -1,19 +1,35 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using CoreCodedChatbot.Library.Interfaces.Services;
 using CoreCodedChatbot.Library.Models.Data;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 
 namespace CoreCodedChatbot.Library.Services
 {
     public class ConfigService : IConfigService
     {
-        public ConfigModel GetConfig()
+        private IConfigurationRoot _configRoot;
+
+        public ConfigService()
         {
-            using (var sr = new StreamReader("config.json"))
-            {
-                var configJson = sr.ReadToEnd();
-                return JsonConvert.DeserializeObject<ConfigModel>(configJson);
-            }
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json")
+                .AddEnvironmentVariables();
+
+            _configRoot = builder.Build();
+        }
+
+        public T Get<T>(string configKey)
+        {
+            var configString = _configRoot[$"AppSettings:{configKey}"];
+
+            if (string.IsNullOrWhiteSpace(configString))
+                return default(T);
+
+            return (T)Convert.ChangeType(configString, typeof(T));
         }
     }
 }
